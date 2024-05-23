fb602cbccba64c79c00fc97d696760c8804dbc7c
jkbmrz <jakob.mraz@gmail.com>
2024-05-23 11:46:35 +0200

    rename depthai-nodes to depthai_nodes

--- depthai-nodes/__init__.py
+++
@@ -1,5 +0,0 @@
-# import nodes as needed, e.g.
-#from .postprocess_node1 import PostProcessNode1
-#from .postprocess_node2 import PostProcessNode2
-
-#__all__ = ['PostProcessNode1', 'PostProcessNode2']
