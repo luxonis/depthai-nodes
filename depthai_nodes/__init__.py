1d32a8b628c1ce69c3817d470902c7432f9604c6
jkbmrz <jakob.mraz@gmail.com>
2024-05-23 12:54:19 +0200

    add dncnn3 postprocessing node

--- depthai_nodes/__init__.py
+++ depthai_nodes/__init__.py
@@ -2,5 +2,6 @@
 #from .postprocess_node1 import PostProcessNode1
 #from .postprocess_node2 import PostProcessNode2
 from .postprocessing_node_zero_dce import ZeroDCEParser
+from .postprocessing_node_dncnn3 import DnCNN3Parser
 
 #__all__ = ['PostProcessNode1', 'PostProcessNode2']
