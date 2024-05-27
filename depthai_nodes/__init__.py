784dded08273ecd2e0bdf20c93b1701e37605216
jkbmrz <jakob.mraz@gmail.com>
2024-05-27 13:50:51 +0200

    refactor

--- depthai_nodes/__init__.py
+++
@@ -1,9 +0,0 @@
-# import nodes as needed, e.g.
-#from .postprocess_node1 import PostProcessNode1
-#from .postprocess_node2 import PostProcessNode2
-from .postprocessing_node_zero_dce import ZeroDCEParser
-from .postprocessing_node_dncnn3 import DnCNN3Parser
-from .postprocessing_node_depth_anything import DepthAnythingParser
-from .postprocessing_node_yunet import YuNetParser
-
-#__all__ = ['PostProcessNode1', 'PostProcessNode2']
---
+++ ml/nodes/__init__.py
@@ -0,0 +1,11 @@
+from .zero_dce import ZeroDCEParser
+from .dncnn3 import DnCNN3Parser
+from .depth_anything import DepthAnythingParser
+from .yunet import YuNetParser
+
+__all__ = [
+    'ZeroDCEParser', 
+    'DnCNN3Parser',
+    'DepthAnythingParser',
+    'YuNetParser'
+    ]
