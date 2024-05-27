1369d3d7397ac218ed80ffbec6052f9c00d4e398
jkbmrz <jakob.mraz@gmail.com>
2024-05-27 14:34:07 +0200

    update ImgDetection(s)WithKeypoints messages

--- ml/messages/img_detections.py
+++ ml/messages/img_detections.py
@@ -1,6 +1,12 @@
 import depthai as dai
+from typing import List
 
-class ImgDetectionsWithKeypoints(dai.ImgDetections):
+class ImgDetectionWithKeypoints(dai.ImgDetection):
     def __init__(self):
-        dai.ImgDetections.__init__(self)
-        self.keypoints = []
+        dai.ImgDetection.__init__(self)
+        self.keypoints = [] # TODO: how to enforce type checking for keypoints? e.g. this currently accept strings
+
+class ImgDetectionsWithKeypoints(dai.Buffer):
+    def __init__(self):
+        dai.Buffer.__init__(self)
+        self.detections: List[ImgDetectionWithKeypoints] = [] # TODO: how to enforce type checking for ImgDetectionWithKeypoints? e.g. this currently accept ImgDetection
--- ml/postprocessing/yunet.py
+++ ml/postprocessing/yunet.py
@@ -2,7 +2,7 @@ import depthai as dai
 import numpy as np
 import cv2
 
-from ..custom_messages.img_detections import ImgDetectionsWithKeypoints
+from ..messages.img_detections import ImgDetectionsWithKeypoints
 
 class YuNetParser(dai.node.ThreadedHostNode):
     def __init__(
