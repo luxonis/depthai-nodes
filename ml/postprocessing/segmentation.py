bf463a2b28a6b65c13ef2624a091687a49a0ad32
kkeroo <61207502+kkeroo@users.noreply.github.com>
2024-05-31 12:40:29 +0200

    general multiclass segmentation parser.

--- ml/postprocessing/segmentation.py
+++ ml/postprocessing/segmentation.py
@@ -3,26 +3,31 @@ import numpy as np
 import cv2
 from .utils.message_creation import create_segmentation_message
 
-class MPSeflieSegParser(dai.node.ThreadedHostNode):
+class SegmentationParser(dai.node.ThreadedHostNode):
     def __init__(
         self,
         threshold=0.5,
+        num_classes=2,
     ):
         dai.node.ThreadedHostNode.__init__(self)
         self.input = dai.Node.Input(self)
         self.out = dai.Node.Output(self)
 
         self.threshold = threshold
+        self.num_classes = num_classes
 
     def setConfidenceThreshold(self, threshold):
         self.threshold = threshold
 
+    def setNumClasses(self, num_classes):
+        self.num_classes = num_classes
+
     def run(self):
         """
-        Postprocessing logic for MediaPipe Selfie Segmentation model.
+        Postprocessing logic for Segmentation model with `num_classes` classes including background at index 0.
 
         Returns:
-            Segmenation mask with two classes 1 - person, 0 - background.
+            Segmenation mask with `num_classes` classes, 0 - background.
         """
 
         while self.isRunning():
@@ -33,9 +38,12 @@ class MPSeflieSegParser(dai.node.ThreadedHostNode):
                 break  # Pipeline was stopped
 
             segmentation_mask = output.getTensor("output")
-            segmentation_mask = segmentation_mask[0].squeeze() > self.threshold
-            overlay_image = np.zeros((segmentation_mask.shape[0], segmentation_mask.shape[1], 1), dtype=np.uint8)
-            overlay_image[segmentation_mask] = 1
+            segmentation_mask = segmentation_mask[0]  # num_clases x H x W
+            overlay_image = np.zeros((segmentation_mask.shape[1], segmentation_mask.shape[2], 1), dtype=np.uint8)
+
+            for class_id in range(self.num_classes-1):
+                class_mask = segmentation_mask[class_id] > self.threshold
+                overlay_image[class_mask] = class_id + 1
 
             imgFrame = create_segmentation_message(overlay_image)
             self.out.send(imgFrame)
