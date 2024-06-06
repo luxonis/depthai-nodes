0b126c890ece36756ed2585dd84d958cac0e09e3
jkbmrz <jakob.mraz@gmail.com>
2024-06-06 17:21:51 +0200

    fix imports

--- ml/postprocessing/__init__.py
+++ ml/postprocessing/__init__.py
@@ -1,6 +1,6 @@
 from .zero_dce import ZeroDCEParser
 from .dncnn3 import DnCNN3Parser
-from .depth_anything import DepthAnythingParser
+from .monocular_depth import MonocularDepthParser
 from .yunet import YuNetParser
 from .mediapipe_hand_detection import MPHandDetectionParser
 from .mediapipe_hand_landmarker import MPHandLandmarkParser
@@ -10,7 +10,7 @@ from .segmentation import SegmentationParser
 __all__ = [
     'ZeroDCEParser', 
     'DnCNN3Parser',
-    'DepthAnythingParser',
+    'MonocularDepthParser',
     'YuNetParser',
     'MPHandDetectionParser',
     'MPHandLandmarkParser',
