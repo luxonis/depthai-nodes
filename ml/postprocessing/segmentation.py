14a9e4c7f092d91f5cb775a539cd97576c67a21d
kkeroo <61207502+kkeroo@users.noreply.github.com>
2024-06-06 11:17:13 +0200

    Remove unnecessary atributes.

--- ml/postprocessing/segmentation.py
+++ ml/postprocessing/segmentation.py
@@ -6,21 +6,10 @@ from .utils.message_creation import create_segmentation_message
 class SegmentationParser(dai.node.ThreadedHostNode):
     def __init__(
         self,
-        threshold=0.5,
-        num_classes=2,
     ):
         dai.node.ThreadedHostNode.__init__(self)
         self.input = dai.Node.Input(self)
         self.out = dai.Node.Output(self)
-
-        self.threshold = threshold
-        self.num_classes = num_classes
-
-    def setConfidenceThreshold(self, threshold):
-        self.threshold = threshold
-
-    def setNumClasses(self, num_classes):
-        self.num_classes = num_classes
 
     def run(self):
         """
@@ -40,7 +29,6 @@ class SegmentationParser(dai.node.ThreadedHostNode):
             segmentation_mask = output.getTensor("output")
             segmentation_mask = segmentation_mask[0]  # num_clases x H x W
             segmentation_mask = np.vstack((np.zeros((1, segmentation_mask.shape[1], segmentation_mask.shape[2]), dtype=np.float32), segmentation_mask))
-            segmentation_mask[segmentation_mask < self.threshold] = 0
             overlay_image = np.argmax(segmentation_mask, axis=0).reshape(segmentation_mask.shape[1], segmentation_mask.shape[2], 1).astype(np.uint8)
 
             imgFrame = create_segmentation_message(overlay_image)
