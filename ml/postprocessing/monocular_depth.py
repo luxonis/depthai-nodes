9531ac10da444da520c76fa80f858549205c8b53
jkbmrz <jakob.mraz@gmail.com>
2024-06-06 17:21:13 +0200

    add generic monocular depth parser

--- ml/postprocessing/monocular_depth.py
+++ ml/postprocessing/monocular_depth.py
@@ -3,7 +3,7 @@ import depthai as dai
 from .utils.message_creation import create_monocular_depth_msg
 
 
-class DepthAnythingParser(dai.node.ThreadedHostNode):
+class MonocularDepthParser(dai.node.ThreadedHostNode):
     def __init__(self):
         dai.node.ThreadedHostNode.__init__(self)
         self.input = dai.Node.Input(self)
@@ -11,7 +11,7 @@ class DepthAnythingParser(dai.node.ThreadedHostNode):
 
     def run(self):
         """
-        Postprocessing logic for Depth Anything model.
+        Postprocessing logic for a model with monocular depth output (e.g.Depth Anything model).
 
         Returns:
             dai.ImgFrame: uint16, HW depth map.
@@ -24,12 +24,17 @@ class DepthAnythingParser(dai.node.ThreadedHostNode):
             except dai.MessageQueue.QueueException as e:
                 break  # Pipeline was stopped
 
-            output = output.getTensor(
-                "depth"
-            )  # numpy.ndarray of shape (1, 1, 518, 518)
+            output_layer_names = output.getAllLayerNames()
+            if len(output_layer_names) != 1:
+                raise ValueError(
+                    f"Expected 1 output layer, got {len(output_layer_names)}."
+                )
+            output = output.getTensor(output_layer_names[0])
+
+            depth_map = output[0, 0]
 
             depth_message = create_monocular_depth_msg(
-                depth_map=output[0, 0],
+                depth_map,
                 depth_type="relative",
             )
             self.out.send(depth_message)
