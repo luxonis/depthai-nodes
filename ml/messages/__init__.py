5ea5e8d9c8d14b86675f616b2d633f889221f671
kkeroo <61207502+kkeroo@users.noreply.github.com>
2024-06-07 10:21:30 +0200

    Parser for XFeat model added.

--- ml/messages/__init__.py
+++ ml/messages/__init__.py
@@ -1,9 +1,11 @@
 from .img_detections import ImgDetectionWithKeypoints, ImgDetectionsWithKeypoints
 from .keypoints import HandKeypoints, Keypoints
+from .matched_points import MatchedPoints
 
 __all__ = [
     "ImgDetectionWithKeypoints",
     "ImgDetectionsWithKeypoints",
     "HandKeypoints",
     "Keypoints",
+    "MatchedPoints",
 ]
--- ml/messages/creators/__init__.py
+++ ml/messages/creators/__init__.py
@@ -3,6 +3,7 @@ from .segmentation import create_segmentation_message
 from .keypoints import create_hand_keypoints_message, create_animal_keypoints_message
 from .detection import create_detection_message
 from .depth import create_depth_message
+from .matched_points import create_matched_points_message
 
 __all__ = [
     "create_image_message",
@@ -11,4 +12,6 @@ __all__ = [
     "create_detection_message",
     "create_depth_message",
     "create_animal_keypoints_message",
+    "create_matched_points_message",
 ]
+
