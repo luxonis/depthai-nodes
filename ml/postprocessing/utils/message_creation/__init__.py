1fb7272e9b96093c205a4d382ab1dafdbac4bcd3
jkbmrz <jakob.mraz@gmail.com>
2024-06-19 17:00:00 +0200

    fix: remove old depth message imports

--- ml/postprocessing/utils/message_creation/__init__.py
+++ ml/postprocessing/utils/message_creation/__init__.py
@@ -1,5 +1,4 @@
 from .image import create_image_message
-from .depth import create_depth_message
 from .segmentation import create_segmentation_message
 from .keypoints import create_hand_keypoints_message
 from .detection import create_detection_message
@@ -7,7 +6,6 @@ from .monocular_depth import create_monocular_depth_message
 
 __all__ = [
     "create_image_message",
-    "create_depth_message",
     "create_segmentation_message",
     "create_hand_keypoints_message",
     "create_detection_message",
