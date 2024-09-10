6abaaed9cfa3aa1a876565a7a23a68449e56aa80
jkbmrz <jakob.mraz@gmail.com>
2024-09-10 15:15:00 +0200

    refactor: remove mapping.py

--- examples/visualization/visualize.py
+++ examples/visualization/visualize.py
@@ -1,12 +1,31 @@
 import depthai as dai
 
-from .mapping import parser_mapping
+from visualizers import *
 
+visualizers = {
+    "YuNetParser": visualize_detections,
+    "SCRFDParser": visualize_detections,
+    "MPPalmDetectionParser": visualize_detections,
+    "YOLO": visualize_detections,
+    "SSD": visualize_detections,
+    "SegmentationParser": visualize_segmentation,
+    "MLSDParser": visualize_line_detections,
+    "KeypointParser": visualize_keypoints,
+    "HRNetParser": visualize_keypoints,
+    "SuperAnimalParser": visualize_keypoints,
+    "MPHandLandmarkParser": visualize_keypoints,
+    "ClassificationParser": visualize_classification,
+    "ImageOutputParser": visualize_image,
+    "MapOutputParser": visualize_map,
+    "AgeGenderParser": visualize_age_gender,
+    "YOLOExtendedParser": visualize_yolo_extended,
+    "FastSAMParser": visualize_fastsam,
+}
 
 def visualize(
     frame: dai.ImgFrame, message: dai.Buffer, parser_name: str, extraParams: dict
 ):
     """Calls the appropriate visualizer based on the parser name and returns True if the
     pipeline should be stopped."""
-    visualizer = parser_mapping[parser_name]
+    visualizer = visualizers[parser_name]
     return visualizer(frame, message, extraParams)
