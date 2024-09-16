9bdcc606f11c3bbab3639e4ef1d9665483af3c9a
aljazkonec1 <konec.aljaz1@gmail.com>
2024-09-16 10:14:55 +0200

    Updated docstrings and names

--- depthai_nodes/ml/messages/misc.py
+++ depthai_nodes/ml/messages/misc.py
@@ -1,20 +1,6 @@
-from typing import Dict, List, Union
-
 import depthai as dai
 
 from ..messages import Classifications
-
-
-class MiscellaneousMessage(dai.Buffer):
-    def __init__(self):
-        super().__init__()
-        self._data: Dict[str, Union[dai.Buffer, float, List]] = {}
-
-    def setData(self, data: Dict[str, Union[dai.Buffer, float, List]]):
-        self._data = data
-
-    def getData(self) -> Dict[str, Union[dai.Buffer, float, List]]:
-        return self._data
 
 
 class AgeGender(dai.Buffer):
