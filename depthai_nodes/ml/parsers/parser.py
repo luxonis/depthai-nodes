c0557c7d9a6b62e865dc92b6bdb50a73f35c168d
jkbmrz <jakob.mraz@gmail.com>
2024-09-24 17:18:44 +0200

    refactor: base parser

--- depthai_nodes/ml/parsers/parser.py
+++ depthai_nodes/ml/parsers/parser.py
@@ -4,7 +4,21 @@ import depthai as dai
 
 
 class Parser(dai.node.ThreadedHostNode):
-    """Base class for all parsers."""
+    """Base class for neural network output parsers.
+
+    This class serves as a foundation for specific parser implementations used to postprocess the outputs of neural network models.
+    Each parser is attached to a model "head" that governs the parsing process as it contains all the necessary information for the parser to function correctly.
+    Subclasses should implement the `run` method to define the parsing logic.
+
+    Attributes
+    ----------
+    input : Node.Input
+        Node's input. It is a linking point to which the Neural Network's output is linked. It accepts the output of the Neural Network node.
+    out : Node.Output
+        Parser sends the processed network results to this output in a form of DepthAI message. It is a linking point from which the processed network results are retrieved.
+    head_config : Dict
+        A dictionary containing configuration details relevant to the parser, including parameters and settings required for output parsing.
+    """
 
     def __init__(self):
         super().__init__()
@@ -13,17 +27,15 @@ class Parser(dai.node.ThreadedHostNode):
 
         self.head_config: Dict = {}
 
-    def build(self, heads: Union[List, Dict], head_name: str = ""):
-        """Initial build method for all parsers. The method sets the head configuration
-        for the parser.
+    def build(self, heads: Union[List, Dict], head_name: str = None):
+        """Sets the head configuration for the specified head.
 
         Attributes
         ----------
         heads : Union[List, Dict]
-            List of all archive head objects: [<depthai.nn_archive.v1.Head object>, ...]
-            or a dictionary containing the head metadata.
+            List of all head objects of the model: [<depthai.nn_archive.v1.Head object>, ...] or a dictionary containing the head configuration.
         head_name : str
-            The name of the head to use. If multiple heads are available in the archive, the name must be specified.
+            The name of the head to use. If multiple heads are available, the name must be specified.
 
         Returns
         -------
@@ -34,39 +46,40 @@ class Parser(dai.node.ThreadedHostNode):
         if isinstance(heads, list):
             if len(heads) == 0:
                 raise ValueError("No heads available in the nn_archive.")
-
-            head = heads[0]
-
-            if len(heads) > 1 and head_name == "":
-                current_parser = self.__class__.__name__
-                parser_names_in_archive = [head.parser for head in heads]
-                num_matches = parser_names_in_archive.count(current_parser)
-                if num_matches == 0:
-                    raise ValueError(
-                        f"No heads available for {current_parser} in the nn_archive."
-                    )
-                elif num_matches == 1:
-                    head = [head for head in heads if head.parser == current_parser][0]
+            elif len(heads) == 1:
+                head = heads[0]
+            else:
+                if head_name:
+                    head_candidates = [
+                        head
+                        for head in heads
+                        if head.metadata.extraParams["name"] == head_name
+                    ]
+                    if len(head_candidates) == 0:
+                        raise ValueError(
+                            f"No head with name {head_name} specified in nn_archive."
+                        )
+                    if len(head_candidates) > 1:
+                        raise ValueError(
+                            f"Multiple heads with name {head_name} found in nn_archive, please specify a unique name."
+                        )
+                    head = head_candidates[0]
                 else:
-                    raise ValueError(
-                        f"Multiple heads with parser= {current_parser} detected, please specify a head name."
-                    )
-
-            if head_name != "":
-                head_candidates = [
-                    head
-                    for head in heads
-                    if head.metadata.extraParams["name"] == head_name
-                ]
-                if len(head_candidates) == 0:
-                    raise ValueError(
-                        f"No head with name {head_name} specified in nn_archive."
-                    )
-                if len(head_candidates) > 1:
-                    raise ValueError(
-                        f"Multiple heads with name {head_name} found in nn_archive, please specify a unique name."
-                    )
-                head = head_candidates[0]
+                    current_parser = self.__class__.__name__
+                    parser_names_in_archive = [head.parser for head in heads]
+                    num_matches = parser_names_in_archive.count(current_parser)
+                    if num_matches == 0:
+                        raise ValueError(
+                            f"No heads available for {current_parser} in the nn_archive."
+                        )
+                    elif num_matches == 1:
+                        head = [
+                            head for head in heads if head.parser == current_parser
+                        ][0]
+                    else:
+                        raise ValueError(
+                            f"Multiple heads with parser= {current_parser} detected, please specify a head name."
+                        )
 
             parser_name = head.parser
             metadata = head.metadata
@@ -84,7 +97,28 @@ class Parser(dai.node.ThreadedHostNode):
                 head_dictionary.update(metadata.extraParams)
             self.head_config = head_dictionary
 
-        else:
+        elif isinstance(heads, dict):
             self.head_config = heads
 
+        else:
+            raise ValueError(
+                f"Provided heads must be of type Dict or List[Dict] not {type(heads)}."
+            )
+
         return self
+
+    def run(self, **kwargs):
+        """Parses the output from the neural network head.
+
+        This method should be overridden by subclasses to implement the specific parsing logic.
+        It accepts arbitrary keyword arguments for flexibility.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for the parsing process.
+
+        Returns:
+            The parsed output message, as defined by the logic in the subclass.
+        """
+        raise NotImplementedError(
+            "Missing the parsing logic. Implement the run method."
+        )
