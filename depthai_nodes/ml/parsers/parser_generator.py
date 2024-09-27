68669899655ce97409fd8d1801609af27430bd0e
jkbmrz <74824974+jkbmrz@users.noreply.github.com>
2024-09-27 14:19:38 +0200

    Parsers-V2: Refactor Base Parser (#78)
    * refactor: base parser
    
    * fix: error message
    
    * feat: change the build() method to accept a depthai.NNArchive object
    
    * Updated BaseParser, added new Abstraction class called Parser.
    
    * small update on Parser to be part of pipeline
    
    * Parser now returns dict, small chage to error handeling.
    
    * return type fix
    
    * indexing fix.
    
    * docstring updated for BaseParser and other small typo's
    
    * Parser name change to ParserGenerator.
    
    ---------
    
    Co-authored-by: aljazkonec1 <konec.aljaz1@gmail.com>

---
+++ depthai_nodes/ml/parsers/parser_generator.py
@@ -0,0 +1,63 @@
+from typing import Dict
+
+import depthai as dai
+
+from . import *
+from .base_parser import BaseParser
+from .utils import decode_head
+
+
+class ParserGenerator(dai.node.ThreadedHostNode):
+    """General interface for instantiating parsers based on the provided model archive.
+
+    The `build` method creates parsers based on the head information stored in the NN Archive. The method then returns a dictionary of these parsers.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def build(
+        self, nn_archive: dai.NNArchive, head_index: int = None
+    ) -> Dict[int, BaseParser]:
+        """Instantiates parsers based on the provided model archive.
+
+        Attributes
+        ----------
+        nn_archive: dai.NNArchive
+            NN Archive of the model.
+        head__index: int
+            Index of the head to be used for parsing. If not provided, each head will instantiate a separate parser.
+
+        Returns
+        -------
+        parsers: Dict[int : BaseParser]
+            A dictionary of instantiated parsers.
+        """
+        heads = nn_archive.getConfig().getConfigV1().model.heads
+        indexes = range(len(heads))
+
+        if len(heads) == 0:
+            raise ValueError("No heads defined in the NN Archive.")
+
+        if head_index:
+            heads = [heads[head_index]]
+            indexes = [head_index]
+
+        parsers = {}
+        pipeline = self.getParentPipeline()
+
+        for index, head in zip(indexes, heads):
+            parser_name = head.parser
+            parser = globals().get(parser_name)
+
+            if parser is None:
+                raise ValueError(f"Parser {parser_name} not a valid parser class.")
+            elif not issubclass(parser, BaseParser):
+                raise ValueError(
+                    f"Parser {parser_name} does not inherit from BaseParser class."
+                )
+
+            head = decode_head(head)
+            parsers[index] = pipeline.create(parser).build(head)
+
+        return parsers
--- depthai_nodes/ml/parsers/utils/__init__.py
+++ depthai_nodes/ml/parsers/utils/__init__.py
@@ -1,4 +1,5 @@
 from .decode_detections import decode_detections
+from .decode_head import decode_head
 from .denormalize import unnormalize_image
 from .medipipe import generate_anchors_and_decode
 from .ppdet import corners2xyxy, parse_paddle_detection_outputs
@@ -9,4 +10,5 @@ __all__ = [
     "generate_anchors_and_decode",
     "parse_paddle_detection_outputs",
     "corners2xyxy",
+    "decode_head",
 ]
