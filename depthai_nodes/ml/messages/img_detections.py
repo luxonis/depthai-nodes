2758770751845a6c8576786f83f4110cc886318e
kuba <lnotspotl@gmail.com>
2024-09-29 23:06:00 -0400

    Fix yunet data types

--- depthai_nodes/ml/messages/img_detections.py
+++ depthai_nodes/ml/messages/img_detections.py
@@ -97,7 +97,7 @@ class ImgDetectionExtended(dai.ImgDetection):
         self._mask = value
 
 
-class ImgDetectionsExtended(dai.Buffer):
+class ImgDetectionsExtended(dai.ImgDetections):
     """ImgDetectionsExtended class for storing image detections with keypoints.
 
     Attributes
@@ -108,7 +108,7 @@ class ImgDetectionsExtended(dai.Buffer):
 
     def __init__(self):
         """Initializes the ImgDetectionsExtended object."""
-        dai.Buffer.__init__(self)  # TODO: change to super().__init__()?
+        dai.ImgDetections.__init__(self)  # TODO: change to super().__init__()?
         self._detections: List[ImgDetectionExtended] = []
 
     @property
