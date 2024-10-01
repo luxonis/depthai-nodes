3036646eca002b81df702eeb35c4e5e994297b25
Jaša Kerec <61207502+kkeroo@users.noreply.github.com>
2024-10-01 15:45:26 +0200

    Parsers-V2: Remove Hand landmark parser and Hand message. (#91)
    * Build method fix.
    
    * Remove Hand landmark parser.
    
    * Remove hand keypoint message.

--- depthai_nodes/ml/messages/creators/__init__.py
+++ depthai_nodes/ml/messages/creators/__init__.py
@@ -9,7 +9,7 @@ from .detection import (
     create_line_detection_message,
 )
 from .image import create_image_message
-from .keypoints import create_hand_keypoints_message, create_keypoints_message
+from .keypoints import create_keypoints_message
 from .map import create_map_message
 from .regression import create_regression_message
 from .segmentation import create_sam_message, create_segmentation_message
@@ -18,7 +18,6 @@ from .tracked_features import create_tracked_features_message
 __all__ = [
     "create_image_message",
     "create_segmentation_message",
-    "create_hand_keypoints_message",
     "create_detection_message",
     "create_line_detection_message",
     "create_tracked_features_message",
--- depthai_nodes/ml/messages/creators/keypoints.py
+++ depthai_nodes/ml/messages/creators/keypoints.py
@@ -1,88 +1,8 @@
 from typing import List, Union
 
-import depthai as dai
 import numpy as np
 
-from ...messages import HandKeypoints, Keypoint, Keypoints
-
-
-def create_hand_keypoints_message(
-    hand_keypoints: np.ndarray,
-    handedness: float,
-    confidence: float,
-    confidence_threshold: float,
-) -> HandKeypoints:
-    """Create a DepthAI message for hand keypoints detection.
-
-    @param hand_keypoints: Detected 3D hand keypoints of shape (N,3) meaning [...,[x, y, z],...].
-    @type hand_keypoints: np.ndarray
-    @param handedness: Handedness score of the detected hand (left: < 0.5, right > 0.5).
-    @type handedness: float
-    @param confidence: Confidence score of the detected hand.
-    @type confidence: float
-    @param confidence_threshold: Confidence threshold for the present hand.
-    @type confidence_threshold: float
-
-    @return: HandKeypoints message containing the detected hand keypoints, handedness, and confidence score.
-    @rtype: HandKeypoints
-
-    @raise ValueError: If the hand_keypoints are not a numpy array.
-    @raise ValueError: If the hand_keypoints are not of shape (N,3).
-    @raise ValueError: If the hand_keypoints 2nd dimension is not of size E{3}.
-    @raise ValueError: If the handedness is not a float.
-    @raise ValueError: If the confidence is not a float.
-    @raise ValueError: If the confidence_threshold is not a float.
-    """
-
-    if not isinstance(hand_keypoints, np.ndarray):
-        raise ValueError(
-            f"hand_keypoints should be numpy array, got {type(hand_keypoints)}."
-        )
-    if len(hand_keypoints.shape) != 2:
-        raise ValueError(
-            f"hand_keypoints should be of shape (N,3) meaning [...,[x, y, z],...], got {hand_keypoints.shape}."
-        )
-    if hand_keypoints.shape[1] != 3:
-        raise ValueError(
-            f"hand_keypoints 2nd dimension should be of size 3 e.g. [x, y, z], got {hand_keypoints.shape[1]}."
-        )
-    if not isinstance(handedness, float):
-        raise ValueError(f"Handedness should be float, got {type(handedness)}.")
-    if not isinstance(confidence, float):
-        raise ValueError(f"Confidence should be float, got {type(confidence)}.")
-    if not isinstance(confidence_threshold, float):
-        raise ValueError(
-            f"Confidence threshold should be float, got {type(confidence_threshold)}."
-        )
-
-    if handedness < 0 or handedness > 1:
-        raise ValueError(
-            f"Handedness should be between 0 and 1, got handedness {handedness}."
-        )
-
-    if confidence < 0 or confidence > 1:
-        raise ValueError(
-            f"Confidence should be between 0 and 1, got confidence {confidence}."
-        )
-    if confidence_threshold < 0 or confidence_threshold > 1:
-        raise ValueError(
-            f"Confidence threshold should be between 0 and 1, got confidence threshold {confidence_threshold}."
-        )
-
-    hand_keypoints_msg = HandKeypoints()
-    hand_keypoints_msg.handedness = handedness
-    hand_keypoints_msg.confidence = confidence
-    points = []
-    if confidence >= confidence_threshold:
-        for i in range(hand_keypoints.shape[0]):
-            pt = dai.Point3f()
-            pt.x = hand_keypoints[i][0]
-            pt.y = hand_keypoints[i][1]
-            pt.z = hand_keypoints[i][2]
-            points.append(pt)
-    hand_keypoints_msg.keypoints = points
-
-    return hand_keypoints_msg
+from ...messages import Keypoint, Keypoints
 
 
 def create_keypoints_message(
