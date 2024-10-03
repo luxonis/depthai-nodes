4d4cc38768e793adfac3e711e3e8f04032372f69
Jaša Kerec <61207502+kkeroo@users.noreply.github.com>
2024-10-03 12:31:46 +0200

    Refactor Segmentation parser. (#94)

--- depthai_nodes/ml/parsers/segmentation.py
+++ depthai_nodes/ml/parsers/segmentation.py
@@ -1,10 +1,13 @@
+from typing import Any, Dict
+
 import depthai as dai
 import numpy as np
 
 from ..messages.creators import create_segmentation_message
+from .base_parser import BaseParser
 
 
-class SegmentationParser(dai.node.ThreadedHostNode):
+class SegmentationParser(BaseParser):
     """Parser class for parsing the output of the segmentation models.
 
     Attributes
@@ -13,6 +16,8 @@ class SegmentationParser(dai.node.ThreadedHostNode):
         Node's input. It is a linking point to which the Neural Network's output is linked. It accepts the output of the Neural Network node.
     out : Node.Output
         Parser sends the processed network results to this output in a form of DepthAI message. It is a linking point from which the processed network results are retrieved.
+    output_layer_name: str
+        Name of the output layer from which the scores are extracted.
     background_class : bool
         Whether to add additional layer for background.
 
@@ -29,16 +34,53 @@ class SegmentationParser(dai.node.ThreadedHostNode):
     **ValueError**: If the number of dimensions of the output tensor is not E{3}.
     """
 
-    def __init__(self, background_class=False):
+    def __init__(self, output_layer_name: str = "", background_class=False):
         """Initializes the SegmentationParser node.
 
+        @param output_layer_name: Name of the output layer from which the scores are
+            extracted.
+        @type output_layer_name: str
         @param background_class: Whether to add additional layer for background.
         @type background_class: bool
         """
-        dai.node.ThreadedHostNode.__init__(self)
-        self.input = self.createInput()
-        self.out = self.createOutput()
+        super().__init__()
+        self.output_layer_name = output_layer_name
         self.background_class = background_class
+
+    def build(
+        self,
+        head_config: Dict[str, Any],
+    ):
+        """Sets the head configuration for the parser.
+
+        Attributes
+        ----------
+        head_config : Dict
+            The head configuration for the parser.
+
+        Returns
+        -------
+        SegmentationParser
+            Returns the parser object with the head configuration set.
+        """
+
+        output_layers = head_config["outputs"]
+        if len(output_layers) != 1:
+            raise ValueError(
+                f"Only one output layer supported for Segmentation, got {output_layers} layers."
+            )
+        self.output_layer_name = output_layers[0]
+        self.background_class = head_config["background_class"]
+
+        return self
+
+    def setOutputLayerName(self, output_layer_name: str):
+        """Sets the name of the output layer.
+
+        @param output_layer_name: The name of the output layer.
+        @type output_layer_name: str
+        """
+        self.output_layer_name = output_layer_name
 
     def setBackgroundClass(self, background_class):
         """Sets the background class.
@@ -55,14 +97,18 @@ class SegmentationParser(dai.node.ThreadedHostNode):
             except dai.MessageQueue.QueueException:
                 break  # Pipeline was stopped
 
-            output_layer_names = output.getAllLayerNames()
+            layers = output.getAllLayerNames()
 
-            if len(output_layer_names) != 1:
-                print(
-                    f"Expected 1 output layer, got {len(output_layer_names)}. Will take the first one."
+            if len(layers) == 1 and self.output_layer_name == "":
+                self.output_layer_name = layers[0]
+            elif len(layers) != 1 and self.output_layer_name == "":
+                raise ValueError(
+                    f"Expected 1 output layer, got {len(layers)} layers. Please provide the output_layer_name."
                 )
 
-            segmentation_mask = output.getTensor(output_layer_names[0], dequantize=True)
+            segmentation_mask = output.getTensor(
+                self.output_layer_name, dequantize=True
+            )
             if len(segmentation_mask.shape) == 4:
                 segmentation_mask = segmentation_mask[0]
 
