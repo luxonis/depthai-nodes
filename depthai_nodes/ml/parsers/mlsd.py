3a7366f775ce76601a9905a9a30a5d1b22be2b33
Jaša Kerec <61207502+kkeroo@users.noreply.github.com>
2024-10-07 17:15:00 +0200

    Parsers-V2: Refactor Palm detection, SuperAnimal, Lane detection and MLSD (#100)
    * Refactor Palm detection parser.
    
    * Refactor SuperAnimal parser.
    
    * Refactor LaneDetection parser.
    
    * Refactor MLSD parser.
    
    * KeypointParser as parent class to SuperAnimal.
    
    * Store layer names in MLSD parser.

--- depthai_nodes/ml/parsers/mlsd.py
+++ depthai_nodes/ml/parsers/mlsd.py
@@ -1,11 +1,14 @@
+from typing import Any, Dict
+
 import depthai as dai
 import numpy as np
 
 from ..messages.creators import create_line_detection_message
+from .base_parser import BaseParser
 from .utils.mlsd import decode_scores_and_points, get_lines
 
 
-class MLSDParser(dai.node.ThreadedHostNode):
+class MLSDParser(BaseParser):
     """Parser class for parsing the output of the M-LSD line detection model. The parser
     is specifically designed to parse the output of the M-LSD model. As the result, the
     node sends out the detected lines in the form of a message.
@@ -16,6 +19,10 @@ class MLSDParser(dai.node.ThreadedHostNode):
         Node's input. It is a linking point to which the Neural Network's output is linked. It accepts the output of the Neural Network node.
     out : Node.Output
         Parser sends the processed network results to this output in a form of DepthAI message. It is a linking point from which the processed network results are retrieved.
+    output_layer_tpmap : str
+        Name of the output layer containing the tpMap tensor.
+    output_layer_heat : str
+        Name of the output layer containing the heat tensor.
     topk_n : int
         Number of top candidates to keep.
     score_thr : float
@@ -32,6 +39,8 @@ class MLSDParser(dai.node.ThreadedHostNode):
 
     def __init__(
         self,
+        output_layer_tpmap="",
+        output_layer_heat="",
         topk_n=200,
         score_thr=0.10,
         dist_thr=20.0,
@@ -45,12 +54,62 @@ class MLSDParser(dai.node.ThreadedHostNode):
         @param dist_thr: Distance threshold for merging lines.
         @type dist_thr: float
         """
-        dai.node.ThreadedHostNode.__init__(self)
-        self.input = self.createInput()
-        self.out = self.createOutput()
+        super().__init__()
+        self.output_layer_tpmap = output_layer_tpmap
+        self.output_layer_heat = output_layer_heat
+
         self.topk_n = topk_n
         self.score_thr = score_thr
         self.dist_thr = dist_thr
+
+    def build(
+        self,
+        head_config: Dict[str, Any],
+    ):
+        """Sets the head configuration for the parser.
+
+        Attributes
+        ----------
+        head_config : Dict
+            The head configuration for the parser.
+
+        Returns
+        -------
+        MLSDParser
+            Returns the parser object with the head configuration set.
+        """
+
+        output_layers = head_config["outputs"]
+        if len(output_layers) != 2:
+            raise ValueError(
+                f"Only two output layers are supported for MLSDParser, got {len(output_layers)} layers."
+            )
+        for layer in output_layers:
+            if "tpMap" in layer:
+                self.output_layer_tpmap = layer
+            elif "heat" in layer:
+                self.output_layer_heat = layer
+        self.topk_n = head_config["topk_n"]
+        self.score_thr = head_config["score_thr"]
+        self.dist_thr = head_config["dist_thr"]
+
+        return self
+
+    def setOutputLayerTPMap(self, output_layer_tpmap):
+        """Sets the name of the output layer containing the tpMap tensor.
+
+        @param output_layer_tpmap: Name of the output layer containing the tpMap tensor.
+        @type output_layer_tpmap: str
+        """
+        self.output_layer_tpmap = output_layer_tpmap
+
+    def setOutputLayerHeat(self, output_layer_heat):
+        """Sets the name of the output layer containing the heat tensor.
+
+        @param output_layer_heat: Name of the output layer containing the heat tensor.
+        @type output_layer_heat: str
+        """
+        self.output_layer_heat = output_layer_heat
 
     def setTopK(self, topk_n):
         """Sets the number of top candidates to keep.
@@ -77,14 +136,27 @@ class MLSDParser(dai.node.ThreadedHostNode):
         self.dist_thr = dist_thr
 
     def run(self):
+        if self.output_layer_tpmap == "":
+            raise ValueError(
+                "Output layer containing the tpMap tensor is not set. Please use setOutputLayerTPMap method or correct NN archive."
+            )
+        if self.output_layer_heat == "":
+            raise ValueError(
+                "Output layer containing the heat tensor is not set. Please use setOutputLayerHeat method or correct NN archive."
+            )
+
         while self.isRunning():
             try:
                 output: dai.NNData = self.input.get()
             except dai.MessageQueue.QueueException:
                 break  # Pipeline was stopped
 
-            tpMap = output.getTensor("tpMap", dequantize=True).astype(np.float32)
-            heat_np = output.getTensor("heat", dequantize=True).astype(np.float32)
+            tpMap = output.getTensor(self.output_layer_tpmap, dequantize=True).astype(
+                np.float32
+            )
+            heat_np = output.getTensor(self.output_layer_heat, dequantize=True).astype(
+                np.float32
+            )
 
             if len(tpMap.shape) != 4:
                 raise ValueError("Invalid shape of the tpMap tensor. Should be 4D.")
--- depthai_nodes/ml/parsers/superanimal_landmarker.py
+++ depthai_nodes/ml/parsers/superanimal_landmarker.py
@@ -1,11 +1,14 @@
+from typing import Any, Dict
+
 import depthai as dai
 import numpy as np
 
 from ..messages.creators import create_keypoints_message
+from .keypoints import KeypointParser
 from .utils.superanimal import get_pose_prediction
 
 
-class SuperAnimalParser(dai.node.ThreadedHostNode):
+class SuperAnimalParser(KeypointParser):
     """Parser class for parsing the output of the SuperAnimal landmark model.
 
     Attributes
@@ -14,6 +17,10 @@ class SuperAnimalParser(dai.node.ThreadedHostNode):
         Node's input. It is a linking point to which the Neural Network's output is linked. It accepts the output of the Neural Network node.
     out : Node.Output
         Parser sends the processed network results to this output in a form of DepthAI message. It is a linking point from which the processed network results are retrieved.
+    output_layer_name: str
+        Name of the output layer from which the scores are extracted.
+    n_keypoints : int
+        Number of keypoints.
     score_threshold : float
         Confidence score threshold for detected keypoints.
     scale_factor : float
@@ -28,22 +35,49 @@ class SuperAnimalParser(dai.node.ThreadedHostNode):
 
     def __init__(
         self,
+        output_layer_name="",
+        n_keypoints=39,
         score_threshold=0.5,
         scale_factor=256,
     ):
         """Initializes the SuperAnimalParser node.
 
+        @param output_layer_name: Name of the output layer from which the keypoints are
+            extracted.
+        @type output_layer_name: str
+        @param n_keypoints: Number of keypoints.
+        @type n_keypoints: int
         @param score_threshold: Confidence score threshold for detected keypoints.
         @type score_threshold: float
         @param scale_factor: Scale factor to divide the keypoints by.
         @type scale_factor: float
         """
-        dai.node.ThreadedHostNode.__init__(self)
-        self.input = self.createInput()
-        self.out = self.createOutput()
+        super().__init__(output_layer_name, scale_factor, n_keypoints=n_keypoints)
+        self.output_layer_name = output_layer_name
 
         self.score_threshold = score_threshold
-        self.scale_factor = scale_factor
+
+    def build(
+        self,
+        head_config: Dict[str, Any],
+    ):
+        """Sets the head configuration for the parser.
+
+        Attributes
+        ----------
+        head_config : Dict
+            The head configuration for the parser.
+
+        Returns
+        -------
+        SuperAnimalParser
+            Returns the parser object with the head configuration set.
+        """
+
+        super().build(head_config)
+        self.score_threshold = head_config["score_threshold"]
+
+        return self
 
     def setScoreThreshold(self, threshold):
         """Sets the confidence score threshold for detected keypoints.
@@ -53,14 +87,6 @@ class SuperAnimalParser(dai.node.ThreadedHostNode):
         """
         self.score_threshold = threshold
 
-    def setScaleFactor(self, scale_factor):
-        """Sets the scale factor to divide the keypoints by.
-
-        @param scale_factor: Scale factor to divide the keypoints by.
-        @type scale_factor: float
-        """
-        self.scale_factor = scale_factor
-
     def run(self):
         while self.isRunning():
             try:
@@ -68,7 +94,17 @@ class SuperAnimalParser(dai.node.ThreadedHostNode):
             except dai.MessageQueue.QueueException:
                 break  # Pipeline was stopped
 
-            heatmaps = output.getTensor("heatmaps", dequantize=True).astype(np.float32)
+            layers = output.getAllLayerNames()
+
+            if len(layers) == 1 and self.output_layer_name == "":
+                self.output_layer_name = layers[0]
+            elif len(layers) != 1 and self.output_layer_name == "":
+                raise ValueError(
+                    f"Expected 1 output layer, got {len(layers)} layers. Please provide the output_layer_name."
+                )
+            heatmaps = output.getTensor(self.output_layer_name, dequantize=True).astype(
+                np.float32
+            )
 
             if len(heatmaps.shape) == 3:
                 heatmaps = heatmaps.reshape((1,) + heatmaps.shape)
