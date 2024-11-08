3456ffec36c46ae9783eb7184312428b51c7876e
kkeroo <61207502+kkeroo@users.noreply.github.com>
2024-11-08 11:02:35 +0100

    Setting envs correctly.

--- tests/end_to_end/setup_camera_ips.py
+++ tests/end_to_end/setup_camera_ips.py
@@ -1,5 +1,3 @@
-import os
-
 import depthai as dai
 
 devices = dai.Device.getAllAvailableDevices()
@@ -14,7 +12,5 @@ for device in devices:
         RVC2_IP = str(mxid)
 
 # set env variables
-os.environ["RVC2_IP"] = RVC2_IP
-print(f"RVC2 IP/mxid: {os.getenv('RVC2_IP')}")
-os.environ["RVC4_IP"] = RVC4_IP
-print(f"RVC4 IP/mxid: {os.getenv('RVC4_IP')}")
+print(f"export RVC2_IP={RVC2_IP}")
+print(f"export RVC4_IP={RVC4_IP}")
