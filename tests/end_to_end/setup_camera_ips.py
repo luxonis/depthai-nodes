b8ddf636f1e6d7cd66b4ced6a0a55fec2bddce8f
kkeroo <61207502+kkeroo@users.noreply.github.com>
2024-11-08 10:53:50 +0100

    Typo.

--- tests/end_to_end/setup_camera_ips.py
+++ tests/end_to_end/setup_camera_ips.py
@@ -3,16 +3,18 @@ import os
 import depthai as dai
 
 devices = dai.Device.getAllAvailableDevices()
+RVC2_IP = ""
+RVC4_IP = ""
 for device in devices:
     mxid = device.getMxId()
     platform = device.platform.name
-    RVC2_IP = ""
-    RVC4_IP = ""
     if "RVC4" in platform:
-        RVC4_IP = mxid
+        RVC4_IP = str(mxid)
     if "MYRIAD" in platform:
-        RVC2_IP = mxid
+        RVC2_IP = str(mxid)
 
 # set env variables
 os.environ["RVC2_IP"] = RVC2_IP
+print(f"RVC2 IP/mxid: {os.getenv('RVC2_IP')}")
 os.environ["RVC4_IP"] = RVC4_IP
+print(f"RVC4 IP/mxid: {os.getenv('RVC4_IP')}")
