a649d607fc1d458cb8d3bfb3eefb35b8d4814c3b
kkeroo <61207502+kkeroo@users.noreply.github.com>
2024-11-14 12:14:24 +0100

    Better flag.

--- tests/end_to_end/main.py
+++ tests/end_to_end/main.py
@@ -70,7 +70,7 @@ def main():
         f"--platform={rvc_platform}",
         "-v",
         "--tb=no",
-        "-r sx",
+        "-r a",
         "--log-cli-level=DEBUG",
         "--color=yes",
     ]
@@ -82,7 +82,7 @@ def main():
             f"--platform={rvc_platform}",
             "-v",
             "--tb=no",
-            "-r sx",
+            "-r a",
             "--log-cli-level=DEBUG",
             "--color=yes",
         ]
