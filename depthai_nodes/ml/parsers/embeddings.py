5d1b6b4c9a23230b2347cf0cb7f7a83cef6af5d9
kkeroo <61207502+kkeroo@users.noreply.github.com>
2024-11-19 10:13:44 +0100

    Follow the parser structure.

--- depthai_nodes/ml/parsers/embeddings.py
+++ depthai_nodes/ml/parsers/embeddings.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict
 
 import depthai as dai
 
@@ -10,20 +10,33 @@ class EmbeddingsParser(BaseParser):
 
     Attributes
     ----------
+    output_layer_name: str
+        Name of the output layer relevant to the parser.
 
     Output Message/s
     ----------------
-    **Type**: ImgDetectionsExtended
+    **Type**: dai.NNData
 
-    **Description**: Message containing bounding boxes, labels, confidence scores, and keypoints or masks and protos of the detected objects.
+    **Description**: The output layer of the neural network model head.
     """
 
     def __init__(self) -> None:
         """Initialize the EmbeddingsParser node."""
         super().__init__()
-        self.output_layer_names = None
+        self.output_layer_name: str = None
 
-    def build(self, head_config: Dict[str, Any]):
+    def setOutputLayerNames(self, output_layer_name: str) -> None:
+        """Sets the output layer name for the parser.
+
+        @param output_layer_name: The output layer name for the parser.
+        @type output_layer_name: str
+        """
+        if not isinstance(output_layer_name, str):
+            raise ValueError("Output layer name must be a string.")
+
+        self.output_layer_name = output_layer_name
+
+    def build(self, head_config: Dict[str, Any]) -> "EmbeddingsParser":
         """Sets the head configuration for the parser.
 
         @param head_config: The head configuration for the parser.
@@ -32,20 +45,12 @@ class EmbeddingsParser(BaseParser):
         @rtype: EmbeddingsParser
         """
 
-        self.output_layer_names = head_config["outputs"]
+        self.output_layer_name = head_config["outputs"]
         assert (
-            len(self.output_layer_names) == 1
+            len(self.output_layer_name) == 1
         ), "Embeddings head should have only one output layer"
 
         return self
-
-    def setOutputLayerNames(self, output_layer_names: List[str]) -> None:
-        """Sets the output layer names for the parser.
-
-        @param output_layer_names: The output layer names for the parser.
-        @type output_layer_names: List[str]
-        """
-        self.output_layer_names = output_layer_names
 
     def run(self):
         while self.isRunning():
@@ -53,8 +58,9 @@ class EmbeddingsParser(BaseParser):
                 output: dai.NNData = self.input.get()
             except dai.MessageQueue.QueueException:
                 break  # Pipeline was stopped, no more data
+
             # Get all the layer names
-            output_names = self.output_layer_names or output.getAllLayerNames()
+            output_names = self.output_layer_name or output.getAllLayerNames()
             assert (
                 len(output_names) == 1
             ), "Embeddings head should have only one output layer"
