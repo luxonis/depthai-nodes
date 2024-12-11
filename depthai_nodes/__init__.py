d5ad6924af94e3e5dea902a6e5c9378c523ab41e
KlemenSkrlj <47853619+klemen1999@users.noreply.github.com>
2024-12-11 10:24:54 +0100

    Release v0.1.1 (#151)

--- depthai_nodes/__init__.py
+++ depthai_nodes/__init__.py
@@ -2,4 +2,4 @@ from .ml.parsers import *
 from .parser_generator import ParserGenerator  # noqa: F401
 from .parsing_neural_network import ParsingNeuralNetwork  # noqa: F401
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
