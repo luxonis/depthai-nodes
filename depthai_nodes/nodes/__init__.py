458eca3b480654eac52067c4df5806b5b756f230
dominik737 <dominik737@gmail.com>
2025-01-31 22:20:36 +0100

    init.py styling fix

--- depthai_nodes/nodes/__init__.py
+++ depthai_nodes/nodes/__init__.py
@@ -1,7 +1,4 @@
 from .depth_color_transform import DepthColorTransform
 from .depth_merger import DepthMerger
 
-__all__ = [
-  "DepthColorTransform",
-  "DepthMerger"
-]
+__all__ = ["DepthColorTransform", "DepthMerger"]
