2bce93810ea8b4a536862e5651e6af6b80793c61
dominik737 <dominik737@gmail.com>
2025-03-02 17:12:45 +0100

    clip rect method

--- depthai_nodes/utils/viewport_clipper.py
+++ depthai_nodes/utils/viewport_clipper.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Tuple
+from typing import List, Tuple
 
 
 class ViewportClipper:
@@ -13,17 +13,25 @@ class ViewportClipper:
         TOP = 0b1000
 
     @staticmethod
-    def _get_location(x: float, y: float) -> int:
-        location = ViewportClipper._PointLocation.INSIDE.value
-        if x < 0:
-            location |= ViewportClipper._PointLocation.LEFT.value
-        elif x > 1:
-            location |= ViewportClipper._PointLocation.RIGHT.value
-        if y < 0:
-            location |= ViewportClipper._PointLocation.BOTTOM.value
-        elif y > 1:
-            location |= ViewportClipper._PointLocation.TOP.value
-        return location
+    def clip_rect(points: List[Tuple[float, float]]) -> List[Tuple[float, float]]:
+        clipped_points: List[Tuple[float, float]] = []
+        points_len = len(points)
+        for i in range(points_len):
+            current_point = points[i]
+            next_point = points[(i + 1) % points_len]
+            clipped_line_pts = ViewportClipper.clip_line(current_point, next_point)
+            if not clipped_line_pts:
+                continue
+            start, end = clipped_line_pts
+            if start != current_point:
+                clipped_points.append(start)
+            else:
+                clipped_points.append(current_point)
+            if end != next_point:
+                clipped_points.append(end)
+            else:
+                clipped_points.append(next_point)
+        return clipped_points
 
     @staticmethod
     def clip_line(pt1: Tuple[float, float], pt2: Tuple[float, float]):
@@ -79,3 +87,16 @@ class ViewportClipper:
             else:
                 x2, y2 = x, y
                 code2 = ViewportClipper._get_location(x2, y2)
+
+    @staticmethod
+    def _get_location(x: float, y: float) -> int:
+        location = ViewportClipper._PointLocation.INSIDE.value
+        if x < 0:
+            location |= ViewportClipper._PointLocation.LEFT.value
+        elif x > 1:
+            location |= ViewportClipper._PointLocation.RIGHT.value
+        if y < 0:
+            location |= ViewportClipper._PointLocation.BOTTOM.value
+        elif y > 1:
+            location |= ViewportClipper._PointLocation.TOP.value
+        return location
