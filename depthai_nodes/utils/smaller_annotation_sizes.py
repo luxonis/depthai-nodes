ac6b55a0bd74c3b91365dc9d2edda49aaffc0b11
dominik737 <dominik737@gmail.com>
2025-03-05 22:24:40 +0100

    smaller annotation sizes class

---
+++ depthai_nodes/utils/smaller_annotation_sizes.py
@@ -0,0 +1,26 @@
+from depthai_nodes.utils.constants import (
+    SMALLER_DETECTION_BORDER_THICKNESS_PER_RESOLUTION,
+    SMALLER_DETECTION_CORNER_SIZE,
+    SMALLER_KEYPOINT_THICKNESS_PER_RESOLUTION,
+    SMALLER_TEXT_SIZE_PER_HEIGHT,
+)
+
+from .annotation_sizes import AnnotationSizes
+
+
+class SmallerAnnotationSizes(AnnotationSizes):
+    @property
+    def border_thickness(self):
+        return self._get_thickness(SMALLER_DETECTION_BORDER_THICKNESS_PER_RESOLUTION)
+
+    @property
+    def keypoint_thickness(self):
+        return self._get_thickness(SMALLER_KEYPOINT_THICKNESS_PER_RESOLUTION)
+
+    @property
+    def text_size(self):
+        return self._get_size_per_height(SMALLER_TEXT_SIZE_PER_HEIGHT)
+
+    @property
+    def corner_size(self):
+        return SMALLER_DETECTION_CORNER_SIZE
