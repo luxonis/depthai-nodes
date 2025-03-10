82a78afb26131264f5a2b95aff7bd9c2d075e2b0
kkeroo <61207502+kkeroo@users.noreply.github.com>
2025-03-10 11:57:00 +0100

    Defaults and types in conftest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,17 @@
 def pytest_addoption(parser):
-    parser.addoption(
-        "--nn_archive_path", action="store", default="", help="NN archive path"
-    )
-    parser.addoption("--model", action="store", default="", help="Model from the ZOO.")
+    parser.addoption("--nn_archive_path", default="", type=str, help="NN archive path")
+    parser.addoption("--model", default="", help="Model from the ZOO.")
     parser.addoption(
         "--platform",
-        action="store",
         default="",
+        type=str,
         help="RVC platform to run the tests on.",
     )
-    parser.addoption(
-        "--models", action="store", default="", help="Model slug from the ZOO."
-    )
-    parser.addoption("--parsers", action="store", default="", help="Parsers to test.")
+    parser.addoption("--models", default="", type=str, help="Model slug from the ZOO.")
+    parser.addoption("--parsers", default="", type=str, help="Parsers to test.")
     parser.addoption(
         "--duration",
-        action="store",
-        default="",
+        default=10,
+        type=int,
         help="Duration of the test in seconds.",
     )
