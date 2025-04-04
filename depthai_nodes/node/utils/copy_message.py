b14d68d84fc2b7c92123809b7cdfc0932a65b3c6
jkbmrz <jakob.mraz@gmail.com>
2025-04-04 17:12:55 +0200

    feat: add message copying mechanism for dai.ImgFrame

--- depthai_nodes/node/utils/copy_message.py
+++ depthai_nodes/node/utils/copy_message.py
@@ -31,14 +31,32 @@ def copy_message(msg: dai.Buffer) -> dai.Buffer:
 
 
 def _copy(msg: dai.Buffer) -> dai.Buffer:
+
+    def _copy_metadata(msg: dai.Buffer) -> dai.Buffer:
+        msg_type = type(msg)
+        msg_copy = msg_type()
+        if hasattr(msg, "getSequenceNum"):
+            msg_copy.setSequenceNum(msg.getSequenceNum())
+        if hasattr(msg, "getTimestamp"):
+            msg_copy.setTimestamp(msg.getTimestamp())
+        if hasattr(msg, "getTimestampDevice"):
+            msg_copy.setTimestampDevice(msg.getTimestampDevice())
+        if hasattr(msg, "getTransformation"):
+            msg_copy.setTransformation(msg.getTransformation())
+        return msg_copy
+
+    def _copy_img_frame(img_frame: dai.ImgFrame) -> dai.ImgFrame:
+        img_frame_copy = _copy_metadata(img_frame)
+        img_frame_copy.setCvFrame(img_frame.getCvFrame(), img_frame.getType())
+        img_frame_copy.setCategory(img_frame.getCategory())
+        return img_frame_copy
+
     def _copy_img_detection(
         img_det: Union[dai.ImgDetection, dai.SpatialImgDetection],
     ) -> Union[dai.ImgDetection, dai.SpatialImgDetection]:
         assert isinstance(img_det, (dai.ImgDetection, dai.SpatialImgDetection))
-        if isinstance(img_det, dai.ImgDetection):
-            img_det_copy = dai.ImgDetection()
+        img_det_copy = _copy_metadata(img_det)
         if isinstance(img_det, dai.SpatialImgDetection):
-            img_det_copy = dai.SpatialImgDetection()
             img_det_copy.spatialCoordinates = img_det.spatialCoordinates
             img_det_copy.boundingBoxMapping = img_det.boundingBoxMapping
         img_det_copy.xmin = img_det.xmin
@@ -53,20 +71,15 @@ def _copy(msg: dai.Buffer) -> dai.Buffer:
         img_dets: Union[dai.ImgDetections, dai.SpatialImgDetections],
     ) -> Union[dai.ImgDetections, dai.SpatialImgDetections]:
         assert isinstance(img_dets, (dai.ImgDetections, dai.SpatialImgDetections))
-        if isinstance(img_dets, dai.ImgDetections):
-            img_dets_copy = dai.ImgDetections()
-        if isinstance(img_dets, dai.SpatialImgDetections):
-            img_dets_copy = dai.SpatialImgDetections()
+        img_dets_copy = _copy_metadata(img_dets)
         img_dets_copy.detections = [
             _copy_img_detection(img_det) for img_det in img_dets.detections
         ]
-        img_dets_copy.setSequenceNum(img_dets.getSequenceNum())
-        img_dets_copy.setTimestamp(img_dets.getTimestamp())
-        img_dets_copy.setTimestampDevice(img_dets.getTimestampDevice())
-        img_dets_copy.setTransformation(img_dets.getTransformation())
         return img_dets_copy
 
-    if isinstance(msg, (dai.ImgDetection, dai.SpatialImgDetection)):
+    if isinstance(msg, dai.ImgFrame):
+        return _copy_img_frame(msg)
+    elif isinstance(msg, (dai.ImgDetection, dai.SpatialImgDetection)):
         return _copy_img_detection(msg)
     elif isinstance(msg, (dai.ImgDetections, dai.SpatialImgDetections)):
         return _copy_img_detections(msg)
