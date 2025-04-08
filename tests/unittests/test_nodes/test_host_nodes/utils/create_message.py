f61108ce177ad33a5de9bb055818cbaf0a1fad58
jkbmrz <jakob.mraz@gmail.com>
2025-04-08 18:07:30 +0200

    feat: update unittest message creators

--- tests/unittests/test_nodes/test_host_nodes/utils/create_message.py
+++ tests/unittests/test_nodes/test_host_nodes/utils/create_message.py
@@ -4,138 +4,121 @@ import depthai as dai
 import numpy as np
 
 from depthai_nodes import ImgDetectionExtended
-from depthai_nodes.message.creators import (
-    create_classification_message,
-    create_classification_sequence_message,
-    create_cluster_message,
-    create_detection_message,
-    create_image_message,
-    create_keypoints_message,
-    create_line_detection_message,
-    create_map_message,
-    create_regression_message,
-    create_segmentation_message,  # TODO: test?
-)
+import depthai_nodes.message.creators as creators
 
-LABELS = [0, 1, 2, 3, 4]
-LABEL_NAMES = ["a", "b", "c", "d", "e"]
-SCORES = [0.2, 0.4, 0.6, 0.8, 1.0]
-BBOXES = np.array(
-    [
-        [0.00, 0.20, 0.00, 0.20],
-        [0.20, 0.40, 0.20, 0.40],
-        [0.40, 0.60, 0.40, 0.60],
-        [0.60, 0.80, 0.60, 0.80],
-        [0.80, 1.00, 0.80, 1.00],
-    ]
-)
-ANGLES = [0.0, 0.1, 0.0, 0.3, 0.0]
-KEYPOINTS = np.array(
-    [
-        [0.0, 0.0, 0.0],
-        [0.1, 0.1, 0.1],
-        [0.2, 0.2, 0.2],
-        [0.3, 0.3, 0.3],
-        [0.4, 0.4, 0.4],
-    ]
-)
-KEYPOINTS_SCORES = np.array(
-    [
-        [0.0, 0.0, 0.0],
-        [0.1, 0.1, 0.1],
-        [0.2, 0.2, 0.2],
-        [0.3, 0.3, 0.3],
-        [0.4, 0.4, 0.4],
-    ]
-)
-LINES = np.array(
-    [
+CLASSES = ["class1", "class2", "class3"]
+SCORES = [0.0, 0.25, 0.75]
+
+CLASSIFICATION = {
+    "classes": CLASSES,
+    "scores": SCORES,
+    "sequence_num": 3,
+}
+
+COLLECTIONS = {
+    "clusters": [
+        [[0.0, 0.0], [0.1, 0.1]],
+        [[0.2, 0.2], [0.3, 0.3]],
+        [[0.4, 0.4], [0.5, 0.5], [0.6, 0.6]],
+    ],  # two 2-point clusters, and one 3-point cluster
+    "lines": [
         [0.0, 0.0, 0.1, 0.1],
         [0.2, 0.2, 0.3, 0.3],
         [0.4, 0.4, 0.5, 0.5],
-        [0.6, 0.6, 0.7, 0.7],
-        [0.8, 0.8, 0.9, 0.9],
-    ]
-)
-CLUSTERS = [[[0.0, 0.0], [0.1, 0.1], [0.2, 0.2]], [[0.3, 0.3], [0.4, 0.4], [0.5, 0.5]]]
-PREDICTIONS = [0.2, 0.3, 0.5]
+    ],  # three lines
+}
+
+DETECTIONS = {
+    "bboxes": [
+        [0.00, 0.20, 0.00, 0.20],
+        [0.20, 0.40, 0.20, 0.40],
+        [0.40, 0.60, 0.40, 0.60],
+    ],  # three bboxes
+    "angles": [0.0, 0.25, 0.75],
+    "labels": [i for i in range(len(CLASSES))],
+    "scores": SCORES,
+    "keypoints": [
+        [[0.0, 0.0], [0.1, 0.1], [0.2, 0.2]],
+        [[0.3, 0.3], [0.4, 0.4], [0.5, 0.5]],
+        [[0.6, 0.6], [0.7, 0.7], [0.8, 0.8]],
+    ],  # three keypoints for each bbox detection
+    "keypoints_scores": [
+        [0.0, 0.1, 0.2],
+        [0.3, 0.4, 0.5],
+        [0.6, 0.7, 0.8],
+    ],
+}
+
 HEIGHT, WIDTH = 5, 5
-MAX_VALUE = 255
-ARR_2D = np.random.randint(0, MAX_VALUE, (HEIGHT, WIDTH), dtype=np.int16)
-ARR_2D_FLOAT = ARR_2D.astype(np.float32)
-IMG = np.random.randint(0, MAX_VALUE, (HEIGHT, WIDTH, 3), dtype=np.uint8)
-MASKS = np.random.randint(0, MAX_VALUE, (HEIGHT, WIDTH), dtype=np.uint8)
+MAX_VALUE = 50
+ARRAYS = {
+    "2d": np.random.randint(0, MAX_VALUE, (HEIGHT, WIDTH)),  # e.g. mask
+    "3d": np.random.randint(0, MAX_VALUE, (HEIGHT, WIDTH, 3)),  # e.g. image
+}
 
 
 def create_classifications(
-    classes: List[str] = LABEL_NAMES, scores: List[float] = SCORES
+    classes: List[str] = CLASSIFICATION["classes"],
+    scores: List[float] = CLASSIFICATION["scores"],
 ):
-    return create_classification_message(scores, classes)
+    return creators.create_classification_message(classes=classes, scores=scores)
 
 
 def create_classifications_sequence(
-    classes: List[str] = LABEL_NAMES,
-    scores: List[float] = SCORES,
+    classes: List[str] = CLASSIFICATION["classes"],
+    scores: List[float] = [
+        CLASSIFICATION["scores"],
+    ]
+    * CLASSIFICATION["sequence_num"],
 ):
-    return create_classification_sequence_message(scores, classes)
-
-
-def create_clusters(clusters: List[List[List[Union[float, int]]]] = CLUSTERS):
-    return create_cluster_message(clusters)
-
-
-def create_img_detections_extended(
-    bboxes: np.ndarray = BBOXES,
-    scores: np.ndarray = SCORES,
-    angles: np.ndarray = ANGLES,
-    labels: np.ndarray = LABELS,
-    keypoints: np.ndarray = KEYPOINTS,
-    keypoints_scores: np.ndarray = KEYPOINTS_SCORES,
-    masks: np.ndarray = MASKS,
-):
-    return create_detection_message(
-        bboxes,
-        scores,
-        angles,
-        labels,
-        keypoints,
-        keypoints_scores,
-        masks,
+    return creators.create_classification_sequence_message(
+        classes=classes, scores=scores
     )
 
 
-def create_img_frame(img: np.ndarray = IMG, dtype=dai.ImgFrame.Type.BGR888p):
-    return create_image_message(img, dtype)
+def create_clusters(
+    clusters: List[List[List[Union[float, int]]]] = COLLECTIONS["clusters"],
+):
+    return creators.create_cluster_message(clusters=clusters)
+
+
+def create_img_frame(
+    image: np.ndarray = ARRAYS["3d"].astype(np.uint8),
+    img_frame_type=dai.ImgFrame.Type.BGR888p,
+):
+    return creators.create_image_message(image=image, img_frame_type=img_frame_type)
 
 
 def create_keypoints(
-    keypoints: np.ndarray = KEYPOINTS,
-    scores: np.ndarray = KEYPOINTS_SCORES,
+    keypoints: List[List[float]] = DETECTIONS["keypoints"][0],
+    scores: List[float] = DETECTIONS["keypoints_scores"][0],
 ):
-    return create_keypoints_message(keypoints, scores)
+    return creators.create_keypoints_message(keypoints=keypoints, scores=scores)
 
 
 def create_lines(
-    lines: np.ndarray = LINES,
-    scores: np.ndarray = SCORES,
+    lines: np.ndarray = np.array(COLLECTIONS["lines"]),
+    scores: np.ndarray = np.array(SCORES),
 ):
-    return create_line_detection_message(lines, scores)
+    return creators.create_line_detection_message(lines=lines, scores=scores)
 
 
-def create_map(map: np.ndarray = ARR_2D_FLOAT):
-    return create_map_message(map)
+def create_map(map: np.ndarray = ARRAYS["2d"].astype(np.float32)):
+    return creators.create_map_message(map=map)
 
 
-def create_regression(predictions: List[float] = PREDICTIONS):
-    return create_regression_message(predictions)
+def create_regression(predictions: List[float] = SCORES):
+    return creators.create_regression_message(predictions=predictions)
 
 
-def create_segmentation(mask: np.ndarray = ARR_2D):
-    return create_segmentation_message(mask)
+def create_segmentation(mask: np.ndarray = ARRAYS["2d"].astype(np.int16)):
+    return creators.create_segmentation_message(mask=mask)
 
 
 def create_img_detection(
-    bbox: np.ndarray = BBOXES[0], label: int = LABELS[0], score: float = SCORES[0]
+    bbox: List[float] = DETECTIONS["bboxes"][0],
+    label: int = DETECTIONS["labels"][0],
+    score: float = DETECTIONS["scores"][0],
 ):
     """Creates a dai.ImgDetection object.
 
@@ -153,7 +136,9 @@ def create_img_detection(
 
 
 def create_img_detections(
-    bboxs: np.ndarray = BBOXES, labels: List = LABELS, scores: List = SCORES
+    bboxs: List[List[float]] = DETECTIONS["bboxes"],
+    labels: List[int] = DETECTIONS["labels"],
+    scores: List[float] = DETECTIONS["scores"],
 ):
     """Creates a dai.ImgDetections object.
 
@@ -172,7 +157,9 @@ def create_img_detections(
 
 
 def create_img_detection_extended(
-    bbox: np.ndarray = BBOXES[0], label: int = LABELS[0], score: float = SCORES[0]
+    bbox: List[float] = DETECTIONS["bboxes"][0],
+    label: int = DETECTIONS["labels"][0],
+    score: float = DETECTIONS["scores"][0],
 ):
     """Creates a ImgDetectionExtended object.
 
@@ -192,3 +179,23 @@ def create_img_detection_extended(
     img_det_ext.confidence = score
     img_det_ext.rotated_rect = (x_center, y_center, width, height, 0)
     return img_det_ext
+
+
+def create_img_detections_extended(
+    bboxes: np.ndarray = np.array(DETECTIONS["bboxes"]),
+    scores: np.ndarray = np.array(DETECTIONS["scores"]),
+    angles: np.ndarray = np.array(DETECTIONS["angles"]),
+    labels: np.ndarray = np.array(DETECTIONS["labels"]),
+    keypoints: np.ndarray = np.array(DETECTIONS["keypoints"]),
+    keypoints_scores: np.ndarray = np.array(DETECTIONS["keypoints_scores"]),
+    masks: np.ndarray = ARRAYS["2d"].astype(np.int16),
+):
+    return creators.create_detection_message(
+        bboxes=bboxes,
+        scores=scores,
+        angles=angles,
+        labels=labels,
+        keypoints=keypoints,
+        keypoints_scores=keypoints_scores,
+        masks=masks,
+    )
