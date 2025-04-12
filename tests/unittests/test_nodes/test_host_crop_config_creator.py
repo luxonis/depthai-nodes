eb4af02da20dd9710d4929a32c71e54f660a9e0c
aljazkonec1 <konec.aljaz1@gmail.com>
2025-04-12 14:29:54 +0200

    Update test

--- tests/unittests/test_nodes/test_host_crop_config_creator.py
+++ tests/unittests/test_nodes/test_host_crop_config_creator.py
@@ -1,3 +1,6 @@
+import time
+from typing import Callable, Dict, Optional, Tuple, Union
+
 import depthai as dai
 import numpy as np
 import pytest
@@ -6,12 +9,17 @@ from conftest import Output
 from pytest import FixtureRequest
 
 from depthai_nodes import ImgDetectionExtended, ImgDetectionsExtended
-from depthai_nodes.node import CropConfigsCreatorNode
+from depthai_nodes.node import CropConfigsCreator
+
+
+@pytest.fixture(scope="session")
+def duration(request):
+    return request.config.getoption("--duration")
 
 
 @pytest.fixture
 def crop_configs_creator():
-    return CropConfigsCreatorNode()
+    return CropConfigsCreator()
 
 
 @pytest.fixture
@@ -77,39 +85,130 @@ def create_img_detections_extended():
     return detections
 
 
+def img_detections_extended_identity_map(detections: ImgDetectionsExtended):
+    return detections
+
+
+def cast_to_dai_img_detections(
+    img_detections_extended: ImgDetectionsExtended,
+) -> dai.ImgDetections:
+    img_detections = dai.ImgDetections()
+    converted_detections = []
+
+    for det in img_detections_extended.detections:
+        converted_det = dai.ImgDetection()
+        converted_det.label = det.label
+        converted_det.confidence = det.confidence
+        xmin, ymin, xmax, ymax = det.rotated_rect.getOuterRect()
+        converted_det.xmin = xmin
+        converted_det.ymin = ymin
+        converted_det.xmax = xmax
+        converted_det.ymax = ymax
+        converted_detections.append(converted_det)
+
+    img_detections.detections = converted_detections
+    img_detections.setSequenceNum(img_detections_extended.getSequenceNum())
+    img_detections.setTimestamp(img_detections_extended.getTimestamp())
+    img_detections.setTransformation(img_detections_extended.transformation)
+
+    return img_detections
+
+
+def verify_resized_crop(crop_config: Dict, target_width: int, target_height: int):
+    assert not crop_config["skipCurrentImage"]
+    assert crop_config["reusePreviousImage"]
+    assert np.isclose(crop_config["base"]["outputHeight"], target_height)
+    assert np.isclose(crop_config["base"]["outputWidth"], target_width)
+
+
+def verify_output_detection(
+    gt_detection: ImgDetectionExtended, return_detection: ImgDetectionExtended
+):
+    gt_coords = gt_detection.rotated_rect.getOuterRect()
+    return_coords = return_detection.rotated_rect.getOuterRect()
+
+    assert np.allclose(gt_coords, return_coords, atol=1e-6)
+    assert return_detection.label == gt_detection.label
+    assert np.isclose(return_detection.confidence, gt_detection.confidence, atol=1e-6)
+    assert np.isclose(
+        return_detection.rotated_rect.angle, gt_detection.rotated_rect.angle, atol=1e-6
+    )
+
+
+def verify_crop_det_assertions(
+    crop_detections: ImgDetectionsExtended, img_detections: ImgDetectionsExtended
+):
+    assert crop_detections.getSequenceNum() == img_detections.getSequenceNum()
+    assert crop_detections.getTimestamp() == img_detections.getTimestamp()
+    assert crop_detections.transformation == img_detections.transformation
+
+
+def load_crop_dict(config_msg) -> Dict:
+    config_str = str(config_msg)
+    config_str = config_str.replace("[", "")
+    config_str = config_str.replace("]", "")
+    config_dict = yaml.safe_load(config_str)
+
+    return config_dict
+
+
 @pytest.mark.parametrize(
     "detections",
     [
         "empty_img_detections_extended",
         "single_img_detections_extended",
         "img_detections_extended",
+        "img_detections_with_map",
+    ],
+)
+@pytest.mark.parametrize(
+    "target_size",
+    [
+        None,
+        (640, 480),
+        (1280, 720),
+    ],
+)
+@pytest.mark.parametrize(
+    "preprocess_function",
+    [
+        img_detections_extended_identity_map,
+        cast_to_dai_img_detections,
     ],
 )
 def test_img_detections(
-    crop_configs_creator: CropConfigsCreatorNode,
+    duration: int,
+    crop_configs_creator: CropConfigsCreator,
     request: FixtureRequest,
     detections: str,
+    target_size: Optional[Tuple[int, int]],
+    preprocess_function: Callable[
+        [ImgDetectionsExtended], Union[ImgDetectionsExtended, dai.ImgDetections]
+    ],
 ):
+    source_size = (1000, 1000)
     img_detections: ImgDetectionsExtended = request.getfixturevalue(detections)
-    img_detections_msg = Output()
 
+    img_detections_msg = Output()
     crop_configs_creator.build(
-        detections_input=img_detections_msg, w=1000, h=1000, target_w=640, target_h=480
+        detections_input=img_detections_msg,
+        source_size=source_size,
+        target_size=target_size,
     )
 
-    assert crop_configs_creator.w == 1000
-    assert crop_configs_creator.h == 1000
-    assert crop_configs_creator.target_w == 640
-    assert crop_configs_creator.target_h == 480
-    assert crop_configs_creator.n_detections == 100
+    assert crop_configs_creator.w == source_size[0]
+    assert crop_configs_creator.h == source_size[1]
+    if target_size is not None:
+        assert crop_configs_creator.target_w == target_size[0]
+        assert crop_configs_creator.target_h == target_size[1]
+    assert crop_configs_creator.resize_mode == dai.ImageManipConfigV2.ResizeMode.STRETCH
 
     q_img_detections_msg = img_detections_msg.createOutputQueue()
 
     q_configs = crop_configs_creator.config_output.createOutputQueue()
     q_detections = crop_configs_creator.detections_output.createOutputQueue()
 
-    img_detections_msg.send(img_detections)
-
+    img_detections_msg.send(preprocess_function(img_detections))
     crop_configs_creator.process(q_img_detections_msg.get())
 
     skip_config: dai.ImageManipConfigV2 = q_configs.get()
@@ -118,118 +217,38 @@ def test_img_detections(
     assert config_dict["skipCurrentImage"]
 
     crop_detections: ImgDetectionsExtended = q_detections.get()
-    assert crop_detections.getSequenceNum() == img_detections.getSequenceNum()
-    assert crop_detections.getTimestamp() == img_detections.getTimestamp()
-    assert crop_detections.transformation == img_detections.transformation
+    verify_crop_det_assertions(crop_detections, img_detections)
 
     for true_det, crop_det in zip(
         img_detections.detections, crop_detections.detections
     ):
-        true_coords = true_det.rotated_rect.getOuterRect()
-        return_coords = crop_det.rotated_rect.getOuterRect()
-
         config_msg = q_configs.get()
-        config_str = str(config_msg)
-        config_str = config_str.replace("[", "")
-        config_str = config_str.replace("]", "")
-        config_dict = yaml.safe_load(config_str)
+        config_dict = load_crop_dict(config_msg)
 
-        assert not config_dict["skipCurrentImage"]
-        assert config_dict["reusePreviousImage"]
-        assert np.isclose(config_dict["base"]["outputHeight"], 480)
-        assert np.isclose(config_dict["base"]["outputWidth"], 640)
+        verify_output_detection(true_det, crop_det)
+        if target_size is not None:
+            verify_resized_crop(config_dict, target_size[0], target_size[1])
 
-        assert np.allclose(true_coords, return_coords, atol=1e-8)
-        assert crop_det.label == true_det.label
-        assert crop_det.confidence == true_det.confidence
+    if duration:
+        start_time = time.time()
 
+        while time.time() - start_time < duration:
+            img_detections_msg.send(preprocess_function(img_detections))
+            crop_configs_creator.process(q_img_detections_msg.get())
+            skip_config: dai.ImageManipConfigV2 = q_configs.get()
+            config_str = str(skip_config)
+            config_dict = yaml.safe_load(config_str)
+            assert config_dict["skipCurrentImage"]
 
-@pytest.mark.parametrize(
-    "detections",
-    [
-        "img_detections_with_map",
-    ],
-)
-def test_map_clipping(
-    crop_configs_creator: CropConfigsCreatorNode,
-    request: FixtureRequest,
-    detections: str,
-):
-    img_detections: ImgDetectionsExtended = request.getfixturevalue(detections)
-    img_detections_msg = Output()
+            crop_detections: ImgDetectionsExtended = q_detections.get()
+            verify_crop_det_assertions(crop_detections, img_detections)
 
-    crop_configs_creator.build(
-        detections_input=img_detections_msg,
-        w=1000,
-        h=1000,
-        target_w=640,
-        target_h=480,
-        n_detections=4,
-    )
+            for true_det, crop_det in zip(
+                img_detections.detections, crop_detections.detections
+            ):
+                config_msg = q_configs.get()
+                config_dict = load_crop_dict(config_msg)
 
-    q_img_detections_msg = img_detections_msg.createOutputQueue()
-
-    q_detections = crop_configs_creator.detections_output.createOutputQueue()
-
-    img_detections_msg.send(img_detections)
-
-    crop_configs_creator.process(q_img_detections_msg.get())
-
-    crop_detections: ImgDetectionsExtended = q_detections.get()
-
-    truth_map = img_detections.masks
-    truth_map = np.where(truth_map >= 4, -1, truth_map)
-
-    assert np.allclose(crop_detections.masks, truth_map)
-
-
-@pytest.mark.parametrize(
-    "detections",
-    [
-        "single_img_detections_extended",
-        "img_detections_extended",
-    ],
-)
-def test_num_detections(
-    crop_configs_creator: CropConfigsCreatorNode,
-    request: FixtureRequest,
-    detections: str,
-):
-    img_detections: ImgDetectionsExtended = request.getfixturevalue(detections)
-    img_detections_msg = Output()
-
-    crop_configs_creator.build(
-        detections_input=img_detections_msg,
-        w=1000,
-        h=1000,
-        target_w=640,
-        target_h=480,
-        n_detections=1,
-    )
-
-    assert crop_configs_creator.w == 1000
-    assert crop_configs_creator.h == 1000
-    assert crop_configs_creator.target_w == 640
-    assert crop_configs_creator.target_h == 480
-    assert crop_configs_creator.n_detections == 1
-
-    q_img_detections_msg = img_detections_msg.createOutputQueue()
-
-    q_configs = crop_configs_creator.config_output.createOutputQueue()
-    q_detections = crop_configs_creator.detections_output.createOutputQueue()
-
-    img_detections_msg.send(img_detections)
-
-    crop_configs_creator.process(q_img_detections_msg.get())
-
-    skip_config: dai.ImageManipConfigV2 = q_configs.get()
-    config_str = str(skip_config)
-    config_dict = yaml.safe_load(config_str)
-    assert config_dict["skipCurrentImage"]
-
-    crop_detections: ImgDetectionsExtended = q_detections.get()
-    assert crop_detections.getSequenceNum() == img_detections.getSequenceNum()
-    assert crop_detections.getTimestamp() == img_detections.getTimestamp()
-    assert crop_detections.transformation == img_detections.transformation
-    assert len(crop_detections.detections) == 1
-    assert len(q_configs.get_all()) == 1
+                verify_output_detection(true_det, crop_det)
+                if target_size is not None:
+                    verify_resized_crop(config_dict, target_size[0], target_size[1])
