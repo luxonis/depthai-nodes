e538cad7397998319e1477fb35820f2ca6351ec9
KlemenSkrlj <47853619+klemen1999@users.noreply.github.com>
2025-05-21 10:28:37 +0200

    Feat/snaps producer (#214)
    * first draft
    
    * fixed datatypes
    
    * finalized node and added tests
    
    * removed extra comment
    
    * added token to fixtures
    
    * bump to dai 3.0.0a15
    
    * small improvements
    
    * test fix

--- depthai_nodes/node/__init__.py
+++ depthai_nodes/node/__init__.py
@@ -30,6 +30,7 @@ from .parsers.xfeat import XFeatMonoParser, XFeatStereoParser
 from .parsers.yolo import YOLOExtendedParser
 from .parsers.yunet import YuNetParser
 from .parsing_neural_network import ParsingNeuralNetwork
+from .snaps_producer import SnapsProducer, SnapsProducerFrameOnly
 from .tiles_patcher import TilesPatcher
 from .tiling import Tiling
 
@@ -68,5 +69,7 @@ __all__ = [
     "ImgFrameOverlay",
     "ImgDetectionsBridge",
     "ImgDetectionsFilter",
+    "SnapsProducerFrameOnly",
+    "SnapsProducer",
     "BaseHostNode",
 ]
