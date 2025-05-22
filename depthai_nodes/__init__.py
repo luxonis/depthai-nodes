a5b76dd01106220cd4f0d7b3ec8aed899390538d
dominik737 <dominik737@gmail.com>
2025-05-22 18:51:32 +0200

    export host parsing neural network

--- depthai_nodes/__init__.py
+++ depthai_nodes/__init__.py
@@ -1,3 +1,4 @@
+from .host_parsing_neural_network import HostParsingNeuralNetwork  # noqa: F401
 from .ml.parsers import *
 from .parser_generator import ParserGenerator  # noqa: F401
 from .parsing_neural_network import ParsingNeuralNetwork  # noqa: F401
