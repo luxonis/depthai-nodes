3c5fdcfca871ce6f344a0fb6bee2e1a2098099ff
dominik737 <dominik737@gmail.com>
2025-07-17 00:40:30 +0200

    fix added argument to the transparent color

--- depthai_nodes/constants.py
+++ depthai_nodes/constants.py
@@ -4,7 +4,7 @@ PRIMARY_COLOR = dai.Color(
     float(21 / 255), float(127 / 255), float(88 / 255), float(1.0)
 )
 TRANSPARENT_PRIMARY_COLOR = dai.Color(
-    float(21 / 255), float(x=127 / 255), float(88 / 255), float(0.2)
+    float(21 / 255), float(127 / 255), float(88 / 255), float(0.2)
 )
 SECONDARY_COLOR = dai.Color(
     float(240 / 255), float(240 / 255), float(240 / 255), float(1.0)
