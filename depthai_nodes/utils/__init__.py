fe0f0dc5236fb5fd8cc5a4271ddb7bf5053eecf9
dominik737 <dominik737@gmail.com>
2025-07-17 15:28:48 +0200

    separate the drawing logic into the detection drawer

--- depthai_nodes/utils/__init__.py
+++ depthai_nodes/utils/__init__.py
@@ -1,7 +1,13 @@
 from .annotation_helper import AnnotationHelper
 from .annotation_sizes import AnnotationSizes
+from .detection_drawer import DetectionDrawer
+from .smaller_annotation_sizes import SmallerAnnotationSizes
+from .viewport_clipper import ViewportClipper
 
 __all__ = [
     "AnnotationHelper",
     "AnnotationSizes",
+    "SmallerAnnotationSizes",
+    "DetectionDrawer",
+    "ViewportClipper",
 ]
