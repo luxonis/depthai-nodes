6b04126cea8011716bfbfa3790a7734ac21e94a9
Danilo Pejovic <115164734+danilo-pejovic@users.noreply.github.com>
2025-11-18 13:30:51 +0100

    Windows tests (#251)
    Adding window tests, sBOM implementation
    
    ---------
    
    Co-authored-by: Danilo <danilo.pejovic@luxonis.com>
    Co-authored-by: kkeroo <61207502+kkeroo@users.noreply.github.com>

--- tests/end_to_end/setup_camera_ips.py
+++ tests/end_to_end/setup_camera_ips.py
@@ -1,3 +1,5 @@
+import platform
+
 import depthai as dai
 
 devices = dai.Device.getAllAvailableDevices()
@@ -5,12 +7,19 @@ RVC2_IP = ""
 RVC4_IP = ""
 for device in devices:
     mxid = device.getDeviceId()
-    platform = device.platform.name
-    if "RVC4" in platform:
+    dev_platform = device.platform.name
+    if "RVC4" in dev_platform:
         RVC4_IP = str(mxid)
-    if "MYRIAD" in platform:
+    if "MYRIAD" in dev_platform:
         RVC2_IP = str(mxid)
 
-# set env variables
-print(f"export RVC2_IP={RVC2_IP}")
-print(f"export RVC4_IP={RVC4_IP}")
+system = platform.system().lower()
+
+if system == "windows":
+    # For Windows CMD
+    print(f"set RVC2_IP={RVC2_IP}")
+    print(f"set RVC4_IP={RVC4_IP}")
+else:
+    # For Linux / macOS / others
+    print(f"export RVC2_IP={RVC2_IP}")
+    print(f"export RVC4_IP={RVC4_IP}")
