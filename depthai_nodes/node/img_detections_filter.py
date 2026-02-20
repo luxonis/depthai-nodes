566fffc66d1bb88e224dc9a86eaa86cdd3bcb202
petrnovota <petr.novota@luxonis.com>
2026-02-20 11:00:54 +0100

    remove unused imports

--- depthai_nodes/node/img_detections_filter.py
+++ depthai_nodes/node/img_detections_filter.py
@@ -1,6 +1,6 @@
 import warnings
 from dataclasses import dataclass
-from typing import Any, Callable, List, Literal, Optional, Tuple
+from typing import List, Optional, Tuple
 
 import depthai as dai
 import numpy as np
