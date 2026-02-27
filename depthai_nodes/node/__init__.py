ecedcace1d8a3d4870aaf78f77f78fce45dbaeef
kkeroo <61207502+kkeroo@users.noreply.github.com>
2026-02-27 12:33:05 +0100

    Back.

--- depthai_nodes/node/__init__.py
+++ depthai_nodes/node/__init__.py
@@ -30,8 +30,7 @@ from .parsers.xfeat import XFeatMonoParser, XFeatStereoParser
 from .parsers.yolo import YOLOExtendedParser
 from .parsers.yunet import YuNetParser
 from .parsing_neural_network import ParsingNeuralNetwork
-
-# from .snaps_producer import SnapsProducer, SnapsProducerFrameOnly
+from .snaps_producer import SnapsProducer, SnapsProducerFrameOnly
 from .tiles_patcher import TilesPatcher
 from .tiling import Tiling
 
@@ -70,7 +69,7 @@ __all__ = [
     "ImgFrameOverlay",
     "ImgDetectionsBridge",
     "ImgDetectionsFilter",
-    # "SnapsProducerFrameOnly",
-    # "SnapsProducer",
+    "SnapsProducerFrameOnly",
+    "SnapsProducer",
     "BaseHostNode",
 ]
