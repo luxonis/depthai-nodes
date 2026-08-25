2b42b6af0841bd79f3442d9d0be76073ed3cc1e7
KlemenSkrlj <47853619+klemen1999@users.noreply.github.com>
2026-08-25 11:09:00 +0200

    Feat/native support non default strides (#308)
    * Remove checks for  non default strides
    
    * DAI version bump

--- depthai_nodes/node/parser_generator.py
+++ depthai_nodes/node/parser_generator.py
@@ -4,7 +4,7 @@ from depthai_nodes.logging import get_logger
 from depthai_nodes.node.parsers import *
 from depthai_nodes.node.parsers.base_parser import BaseParser
 from depthai_nodes.node.parsers.utils import decode_head
-from depthai_nodes.node.parsers.utils.yolo import YOLOSubtype, resolve_yolo_strides
+from depthai_nodes.node.parsers.utils.yolo import YOLOSubtype
 
 
 class ParserGenerator(dai.node.ThreadedHostNode):
@@ -75,8 +75,6 @@ class ParserGenerator(dai.node.ThreadedHostNode):
             if parser_name in self.DEVICE_PARSERS:
                 if hostOnly:
                     parser_name = self._getHostParserName(parser_name)
-                elif parser_name == "YOLO" and self._has_non_default_yolo_strides(head):
-                    parser_name = self._getHostParserName(parser_name)
                 else:
                     parser = pipeline.create(dai.node.DetectionParser)
                     parser.setNNArchiveHead(head)
@@ -86,10 +84,7 @@ class ParserGenerator(dai.node.ThreadedHostNode):
                 yolo_subtype_str = head.metadata.subtype
                 if yolo_subtype_str is not None:
                     yolo_subtype = YOLOSubtype(yolo_subtype_str.lower())
-                    if (
-                        yolo_subtype in self.DAI_SUPPORTED_YOLO_SUBTYPES
-                        and not self._has_non_default_yolo_strides(head, yolo_subtype)
-                    ):
+                    if yolo_subtype in self.DAI_SUPPORTED_YOLO_SUBTYPES:
                         parser = pipeline.create(dai.node.DetectionParser)
                         parser.setNNArchiveHead(head)
                         if head.metadata.maskOutputs is not None and is_rvc2_device:
@@ -130,50 +125,6 @@ class ParserGenerator(dai.node.ThreadedHostNode):
             parsers[index] = pipeline.create(parser).build(head_config)
 
         return parsers
-
-    @staticmethod
-    def _has_non_default_yolo_strides(
-        head,
-        subtype: YOLOSubtype | None = None,
-    ) -> bool:
-        head_config = decode_head(head)
-        strides = head_config.get("strides")
-        if strides is None:
-            return False
-
-        metadata = getattr(head, "metadata", None)
-
-        if subtype is None:
-            yolo_subtype_str = getattr(metadata, "subtype", None)
-            if yolo_subtype_str is None:
-                subtype = YOLOSubtype.DEFAULT
-            else:
-                try:
-                    subtype = YOLOSubtype(yolo_subtype_str.lower())
-                except ValueError:
-                    return True
-
-        yolo_outputs = getattr(metadata, "yoloOutputs", None)
-        outputs = (
-            yolo_outputs if yolo_outputs is not None else head_config.get("outputs")
-        )
-        num_outputs = len(outputs or [])
-
-        try:
-            resolved_strides = resolve_yolo_strides(
-                strides,
-                subtype,
-                num_outputs,
-            )
-        except ValueError:
-            return True
-
-        default_strides = resolve_yolo_strides(
-            None,
-            subtype,
-            num_outputs,
-        )
-        return resolved_strides != default_strides
 
     def run(self):
         """No-op required by ``dai.node.ThreadedHostNode``."""
