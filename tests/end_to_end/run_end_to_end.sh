00a10699be635a645bf5f0bc570836185f0fe965
Tomaž Štrus <tomaz.strus@luxonis.com>
2026-08-27 09:39:37 +0200

    change python3 and python3.12 to python

--- tests/end_to_end/run_end_to_end.sh
+++ tests/end_to_end/run_end_to_end.sh
@@ -53,7 +53,7 @@ case "$(uname -s)" in
   Darwin)
     HIL_VENV="venv"
     PYTHON312="$(brew --prefix python@3.12)/bin/python3.12"
-    uv venv --seed --python "$PYTHON312" "$HIL_VENV"
+    uv venv --clear --seed --python "$PYTHON312" "$HIL_VENV"
     # shellcheck disable=SC1091
     source "$HIL_VENV/bin/activate"
     ;;
